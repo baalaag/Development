857a33d9fc29d631f888eef926264b1665e0602f
bala <baalaag28@gmail.com>
2019-12-15 14:01:55 +0530

    divide code

---
+++ src/com/example/Divide.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Divide {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		int c=b/a;
+	System.out.println("sum of numbers"+c);
+	}
+
+}
